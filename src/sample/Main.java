dca9ddce4440d4604d5a5688f78f138cc3013c4f
mikshka29 <korshunovm@mail.ru>
2013-12-17 00:10:57 +0400

    initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,39 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        primaryStage.setTitle("Hello World");
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 250);
+        Button btn = new Button();
+        btn.setLayoutX(100);
+        btn.setLayoutY(80);
+        btn.setText("Hello World");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World");
+            }
+        });
+        root.getChildren().add(btn);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
